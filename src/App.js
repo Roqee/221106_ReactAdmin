708e3a99479080da953e4cd05205f0d91e0a0bfa
Roqee <qiqiang_mu@163.com>
2022-11-06 21:57:03 +0800

    20221106 commit project from Roqee

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Button, message } from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	handClick = () => {
+		message.success('成功啦。。。')
+	}
+
+	render() {
+		return (
+			<div>
+				<Button type='primary' onClick={this.handClick}>
+					点击
+				</Button>
+			</div>
+		)
+	}
 }
-
-export default App;
